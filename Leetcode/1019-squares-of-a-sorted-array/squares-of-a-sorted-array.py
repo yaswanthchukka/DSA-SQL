5823ac50bb68b721e20e0f6e5ef3e3b8837c1187
Chukka Yaswanth kumar <138193061+yaswanthchukka@users.noreply.github.com>
2024-03-02 12:15:41 +0530

    Time: 342 ms (5.07%) | Memory: 13.9 MB (37.19%) - LeetSync

--- Leetcode/1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ Leetcode/1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,11 +1,38 @@
 class Solution(object):
+    def merge(self,arr):
+        if len(arr)>1:
+            mid=len(arr)//2
+            l_arr=arr[:mid]
+            r_arr=arr[mid:]
+            self.merge(l_arr)
+            self.merge(r_arr)
+            i=j=k=0
+            while i<len(l_arr) and j<len(r_arr):
+                if l_arr[i]>r_arr[j]:
+                    arr[k]=r_arr[j]
+                    j+=1
+                else:
+                    arr[k]=l_arr[i]
+                    i+=1
+                k+=1
+            while i<len(l_arr):
+                arr[k]=l_arr[i]
+                i+=1
+                k+=1
+            while j<len(r_arr):
+                arr[k]=r_arr[j]
+                j+=1
+                k+=1
+
+
+
     def sortedSquares(self, nums):
         """
         :type nums: List[int]
         :rtype: List[int]
         """
         l=list(map(lambda x:x*x,nums))
-        l.sort()
+        self.merge(l)
         return l
         
         
