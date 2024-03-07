31a5d5c429abafbb0834ad128c7a6d0088cbe742
Chukka Yaswanth kumar <138193061+yaswanthchukka@users.noreply.github.com>
2024-03-07 08:14:17 +0530

    Time: 36 ms (50.55%) | Memory: 16.6 MB (46.37%) - LeetSync

--- Leetcode/908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ Leetcode/908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -6,18 +6,22 @@
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         l=[]
+        add=[]
         t=head
         while t:
             l.append(t.val)
+            add.append(t)
             t=t.next
         k=len(l)
-        
-        t2=head 
-        c=0
-        while t2:
-            if c==(k//2):
-                break
-            c+=1
-            t2=t2.next
-        return t2
+        if k%2!=0:
+            return add[k//2]
+        return add[(k+1)//2]
+        # t2=head 
+        # c=0
+        # while t2:
+        #     if c==(k//2):
+        #         break
+        #     c+=1
+        #     t2=t2.next
+        # return t2
         
