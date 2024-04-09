b319155d45abdfa8ab611838455beb0311e05218
Chukka Yaswanth kumar <138193061+yaswanthchukka@users.noreply.github.com>
2024-04-10 00:29:57 +0530

    Time: 50 ms (39.39%) | Memory: 16.6 MB (16.09%) - LeetSync

--- Leetcode/2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
+++ Leetcode/2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
@@ -1,8 +1,14 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        k1 = sum(list(filter(lambda x:x<=tickets[k],tickets[:k])))
-        k2 = len(list(filter(lambda x:x>tickets[k],tickets[:k])))
-        k3 = sum(list(filter(lambda x:x<tickets[k],tickets[k+1:])))
-        k4 = len(list(filter(lambda x:x>=tickets[k],tickets[k+1:])))
-        return k1+k3+(k2*tickets[k])+(k4*(tickets[k]-1))+tickets[k]
+        t=0
+        i=0
+        while tickets[k]>0:
+            if tickets[i]>0:
+                tickets[i]-=1
+                t+=1
+            i+=1
+            if i==len(tickets):
+                i=0
+        return t
+
         
