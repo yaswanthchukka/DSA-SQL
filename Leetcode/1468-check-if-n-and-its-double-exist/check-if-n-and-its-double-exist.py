18a7d71f1f8ff3d30d92bbc2392f983e16e73f21
Chukka Yaswanth kumar <138193061+yaswanthchukka@users.noreply.github.com>
2024-12-01 22:21:49 +0530

    Time: 2 ms (60.19%) | Memory: 17.4 MB (5.25%) - LeetSync

--- Leetcode/1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ Leetcode/1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -3,9 +3,12 @@ class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
         i = 0 
         while i < len(arr):
-            k = arr[i]/2
-            if math.ceil(k)==math.floor(k):
-                if k in arr[:i] or k in arr[i+1:]:
+            k1 = arr[i]/2
+            k2 = arr[i]*2 
+            if k2 in arr[:i] or k2 in arr[i+1:]:
+                return True
+            if math.ceil(k1)==math.floor(k1):
+                if k1 in arr[:i] or k1 in arr[i+1:]:
                     return True
             i += 1
         return False
