48b5ec03e1b508ec145dadbf2e73cfee3dfee085
Chukka Yaswanth kumar <138193061+yaswanthchukka@users.noreply.github.com>
2025-01-13 18:36:50 +0530

    Time: 359 ms (12.38%) | Memory: 18.9 MB (14.92%) - LeetSync

--- Leetcode/3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
+++ Leetcode/3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
@@ -1,22 +1,16 @@
 class Solution:
-    def fun(self,l):
-        if len(l) >= 3:
-            l.pop(2)
-            l.pop(0)
-            self.fun(l)
-        return len(l)
     def minimumLength(self, s: str) -> int:
         d = {}
 
         for i in range(len(s)):
             if s[i] not in d:
-                d[s[i]] = [i]
+                d[s[i]] = 1
             else:
-                d[s[i]].append(i)
+                d[s[i]] += 1
 
         res = 0
         for i in d:
-            if len(d[i]) % 2 == 0:
+            if d[i] % 2 == 0:
                 res += 2
             else:
                 res += 1
