3e90703ce0c7c1776e13e107f8ab675eb8cb8230
Chukka Yaswanth kumar <138193061+yaswanthchukka@users.noreply.github.com>
2025-04-09 01:41:20 +0530

    Time: 4 ms (51.60%) | Memory: 17.8 MB (49.11%) - LeetSync

--- Leetcode/3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.py
+++ Leetcode/3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -3,12 +3,12 @@ class Solution:
         arr = [-1]*100
         max_ = -1
         for i in range(len(nums)):
-            if arr[nums[i] - 1] == -1:
-                arr[nums[i] - 1] = i 
-            else:
-                if max_ < arr[nums[i] - 1]:
-                    max_ = arr[nums[i] - 1]
-                arr[nums[i] - 1] = i
+            # if arr[nums[i] - 1] == -1:
+            #     arr[nums[i] - 1] = i 
+            # else:
+            if max_ < arr[nums[i] - 1]:
+                max_ = arr[nums[i] - 1]
+            arr[nums[i] - 1] = i
         if max_ != -1:
             return max_//3 + 1
         return 0
