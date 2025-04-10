326ad50364ee1710fbdc9fe838400e35e6edaa25
Chukka Yaswanth kumar <138193061+yaswanthchukka@users.noreply.github.com>
2025-04-11 00:31:49 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (53.43%) - LeetSync

--- Leetcode/75-sort-colors/sort-colors.py
+++ Leetcode/75-sort-colors/sort-colors.py
@@ -3,6 +3,27 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        c1 = 0
+        c2 = 0
+        c3 = 0
+        for i in nums:
+            if i == 0:
+                c1 += 1
+            elif i == 1:
+                c2 += 1
+            else:
+                c3 += 1
+        k = 0
+        for i in range(c1):
+            nums[k] = 0
+            k += 1
+        for i in range(c2):
+            nums[k] = 1
+            k += 1
+        for i in range(c3):
+            nums[k] = 2
+            k += 1
+        
+        
         
         
