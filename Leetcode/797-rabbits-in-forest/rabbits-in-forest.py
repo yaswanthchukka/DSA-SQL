8900d479adc31811c38b3de837287a53c0971ecb
Chukka Yaswanth kumar <138193061+yaswanthchukka@users.noreply.github.com>
2025-04-20 17:21:59 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (9.16%) - LeetSync

---
+++ Leetcode/797-rabbits-in-forest/rabbits-in-forest.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        d = {}
+        c = 0
+        for i in answers:
+            if i in d:
+                d[i] += 1
+            else:
+                if i == 0:
+                    c += 1
+                    continue
+                d[i] = 1
+        for j in d:
+            if d[j]%(j+1) == 0:
+                c += (d[j]//(j+1))*(j+1)
+            else:
+                c += (d[j]//(j+1) + 1)*(j+1)
+        return c
